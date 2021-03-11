7fef54ed5809791355bb1e654623af787e247661
zehtravassos <zetravassos@gmail.com>
2021-03-11 09:59:11 -0500

    mongo db config. mongoose connect. dotenv install

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+//Conect to DB
+mongoose.connect(
+  'mongodb+srv://zehtra:mongo0013@cluster0.xohxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true },
+  () => console.log('connected to db!')
+);
 //Import Routes
 const authRouter = require('./routes/auth');
 
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
+});
+
 //Middlewares
-app.use('/api/user', authRouter);
+//app.use('/api/user', authRouter);
 
 app.listen(3000, () => console.log('Server up and running'));
